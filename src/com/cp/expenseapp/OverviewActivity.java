8653ac11d1841944bfa11c37527e3ffc8a27d6eb
Vnu <dearvnu@yahoo.in>
2013-09-14 11:09:35 -0700

    Initial Commit of the base App

---
+++ src/com/cp/expenseapp/OverviewActivity.java
@@ -0,0 +1,22 @@
+package com.cp.expenseapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class OverviewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_overview);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.overview, menu);
+		return true;
+	}
+
+}
